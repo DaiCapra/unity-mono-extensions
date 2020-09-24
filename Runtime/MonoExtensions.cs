914d3b3519a2667047cb7255b9267248acbf7983
DaiCapra <daniel@artmann.co>
2020-09-24 06:28:13 +0200

    folders

---
+++ Runtime/MonoExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Runtime
+{
+    public static class MonoExtensions
+    {
+        public static void ValidateChild<TK, TV>(this TK mono, ref TV behaviour, bool includeSelf = true,
+            bool includeInactive = true)
+            where TK : MonoBehaviour
+            where TV : Component
+        {
+            if (mono == null)
+            {
+                return;
+            }
+
+            if (behaviour != null)
+            {
+                return;
+            }
+
+            var c = mono.GetComponentsInChildren<TV>();
+            if (!includeSelf)
+            {
+                var f = c.FirstOrDefault(t => t.transform.Equals(mono.transform));
+                behaviour = f;
+            }
+            else
+            {
+                behaviour = c.FirstOrDefault();
+            }
+        }
+    }
+}
