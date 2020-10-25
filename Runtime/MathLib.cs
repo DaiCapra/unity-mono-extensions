966cfbe2320cc29d7035dd38dc00b4fa3ffb48b1
DaiCapra <daniel@artmann.co>
2020-10-25 03:38:50 +0100

    relative functions

--- Runtime/MathLib.cs
+++ Runtime/MathLib.cs
@@ -1,4 +1,6 @@
-﻿namespace MathLibs.Runtime
+﻿using UnityEngine;
+
+namespace MathLibs.Runtime
 {
     public static class MathLib
     {
@@ -6,6 +8,19 @@
         {
             int r = a % n;
             return r < 0 ? r + n : r;
+        }
+
+        public static Vector3 GetRelativeDirection(Vector3 forward, Vector3 v)
+        {
+            var angle = GetRelativeAngle(forward, v);
+            var direction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * angle), 0, Mathf.Sin(Mathf.Deg2Rad * angle));
+            return direction;
+        }
+
+        public static float GetRelativeAngle(Vector3 forward, Vector3 v)
+        {
+            var angle = Vector3.SignedAngle(forward, v, Vector3.up);
+            return angle;
         }
     }
 }
